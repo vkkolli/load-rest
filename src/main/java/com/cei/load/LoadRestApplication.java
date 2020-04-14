c199d8314bb5e3c033f1ba0169ea30b965cf6e45
Kathir <ukathirvelu@ceiamerica.com>
2020-04-14 18:45:53 +0530

    Added swagger implementation for API documentation.

--- src/main/java/com/cei/load/LoadRestApplication.java
+++ src/main/java/com/cei/load/LoadRestApplication.java
@@ -5,10 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
  * The Class LoadRestApplication.
  */
 @SpringBootApplication
+@EnableSwagger2
 public class LoadRestApplication {
 
 	/**
